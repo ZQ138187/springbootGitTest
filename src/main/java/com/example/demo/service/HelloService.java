6281f11ab9b97c6a00052383d217a03fade4a234
zhengqing <zhengqing233@126.com>
2020-12-10 17:19:06 +0800

    s1

---
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: TODO
+ * @Author: zq
+ * @Date: 2020/12/10 17:18
+ */
+@Service
+public class HelloService {
+}
