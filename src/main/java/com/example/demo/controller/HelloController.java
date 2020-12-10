2916c54bbc669870f9e0821d4315fe427360f6ca
zhengqing <zhengqing233@126.com>
2020-12-10 17:18:08 +0800

    c1

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description: TODO
+ * @Author: zq
+ * @Date: 2020/12/10 17:17
+ */
+@Controller
+public class HelloController {
+}
